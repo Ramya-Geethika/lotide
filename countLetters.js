79c879bbf039206292d2dcae003fb11a791b1def
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-19 19:55:50 +0000

    Counting the letters

---
+++ countLetters.js
@@ -0,0 +1,48 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const countLetters = function(sentence) {
+  const result = {};
+  let array = sentence.split('');
+  // console.log(array);
+  for (const item of array) {
+    if (item !== ' ') {
+      result[item] = 0;
+    }
+  }
+  for (const item of array) {
+    if (item !== ' ') {
+      result[item] += 1;
+    }
+  }
+  return result;
+
+};
+
+console.log(countLetters("lighthouse in the house"));
+
+/*{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+} */
+
+
+
