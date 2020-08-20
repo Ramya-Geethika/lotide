b663d38237afa9c1365ad6616066d5e2fae5c17b
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-20 01:41:15 +0000

    Returning the keys for the object

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let result = 0;
+  for (let item in object) {
+    if (object[item] === value)
+    { result = item; 
+    }
+    else {
+      result = undefined;
+    }
+  }
+  return result;
+};
+// test
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, " "), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Exp"), "sci_fi");
+
+
+
