b46aabf8152ed8dfbb8d7c1503a49dde7d5afce7
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-24 04:28:28 +0000

    returns the middle element

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function(arr1, arr2){
-  if(arr1.length != arr2.length){
-    return false;
-  } else {
-    for(let i=0; i< arr1.length; i++){
-      if(arr1[i] != arr2[i]){
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-     return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const eqArrays = require('./eqArrays.js');
 
 const middle = function(array) {
   let mid = 0;
@@ -38,16 +19,12 @@ const middle = function(array) {
     return result;
   } 
 };
+module.exports = middle;
 
 
 
  
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]); 
-assertArraysEqual(middle([]),[]); 
-assertArraysEqual(middle([1,2]),[]); 
-assertArraysEqual(middle([1]),[]); 
-assertArraysEqual(middle([1, 3, 4, 5, 6 ,7, 8, 9]),[5, 6]);
+
 
 
 
