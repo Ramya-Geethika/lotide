58cf5174f7d3550096e7bda14d340558cc7dc4a0
Ramya-Geethika <57380205+Ramya-Geethika@users.noreply.github.com>
2020-08-24 09:14:17 -0400

    Update takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -30,8 +30,9 @@ const takeUntil = function(array, callback) {
   }
   return result;
 };
+module.exports = takeUntil;
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+/* const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 
@@ -42,4 +43,4 @@ const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
 console.log(assertArraysEqual(results1,  [ 1, 2, 5, 7, 2 ]));
-console.log(assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood' ]));
+console.log(assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood' ])); */
