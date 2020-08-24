e0338027835e39a8432de48eb4b5bede6e38cc67
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-24 04:04:45 +0000

    returns last element of the array

--- tail.js
+++ tail.js
@@ -1,22 +1,12 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+const tail = function(arr) {
+  
+  const last = [];
+  for (let i = 1; i < arr.length; i++) {
+   
+    last.push(arr[i]);
   }
+  return last;
 };
 
-let tail = function(arr) {
-  const result = [];
-  arr.forEach((element, index)=>{
-    if (index !== 0) {
-      result.push(element);
-    }
-  });
-  return result;
-};
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
